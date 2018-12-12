717a87712a853a5a95f25ed338f23309f79929c6
mlboy <mlboy@126.com>
2018-12-12 23:53:23 +0800

    commit

--- service/pay-srv/handler/pay.go
+++ service/pay-srv/handler/pay.go
@@ -98,7 +98,7 @@ func (s *PayService) Create(ctx context.Context, req *pay_srv.CreateReq, rsp *pa
 		order.NotifyURL = "https://test.com"   //异步通知地址
 		order.TradeType = tradeModel.TradeType       //TradeType  (JSAPI|NATIVE)
 
-		if tradeModel.TradeType == "JSAPI" {
+		if tradeModel.TradeType == TradeTypeWxJsApi {
 			//仅在 TradeType=JSAPI 时必须
 			extra := make(map[string]string)
 			rawExtra := []byte(req.GetExtra())
@@ -132,7 +132,7 @@ func (s *PayService) Create(ctx context.Context, req *pay_srv.CreateReq, rsp *pa
 		}
 		rsp.Channel = tradeModel.Channel
 		rsp.ProviderName = tradeModel.ProviderName
-		if tradeModel.TradeType == "JSAPI" {
+		if tradeModel.TradeType == TradeTypeWxJsApi {
 			payConfig := &mch.PayConfigJs{
 				AppID:     order.AppID,
 				TimeStamp: time.Now().Unix(),
