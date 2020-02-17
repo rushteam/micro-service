496252e4177647bc6e27a8ce002ad072e33e5568
mlboy <mlboy@126.com>
2020-02-17 09:13:20 +0800

    wip

--- service/user-srv/main.go
+++ service/user-srv/main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"time"
 
-	cli "github.com/micro/cli/v2"
-	micro "github.com/micro/go-micro/v2"
+	"github.com/micro/cli/v2"
+	"github.com/micro/go-micro/v2"
 	"github.com/rushteam/micro-service/common/micro/wrap"
 	"github.com/rushteam/micro-service/service/user-srv/handler"
 	"upper.io/db.v3/mysql"
