ed12582db76ddf5d64a2443a7ae13ae3c63a274a
mlboy <mlboy@126.com>
2020-02-27 22:01:05 +0800

    调整逻辑

--- common/pb/usersrv/user.pb.micro.go
+++ common/pb/usersrv/user.pb.micro.go
@@ -34,12 +34,14 @@ var _ server.Option
 // Client API for UserService service
 
 type UserService interface {
-	//用户登录(账号+密码)
-	Signin(ctx context.Context, in *SigninReq, opts ...client.CallOption) (*AuthRsp, error)
 	//用户注册
 	Signup(ctx context.Context, in *SignupReq, opts ...client.CallOption) (*UserInfo, error)
+	//用户登录(账号+密码)
+	Signin(ctx context.Context, in *SigninReq, opts ...client.CallOption) (*AuthRsp, error)
 	//用户登录(手机号+验证码)
 	SigninByPhoneCaptcha(ctx context.Context, in *SigninByPhoneCaptchaReq, opts ...client.CallOption) (*AuthRsp, error)
+	//第三方授权登陆 OAuth
+	Authorize(ctx context.Context, in *AuthorizeReq, opts ...client.CallOption) (*AuthRsp, error)
 }
 
 type userService struct {
@@ -54,9 +56,9 @@ func NewUserService(name string, c client.Client) UserService {
 	}
 }
 
-func (c *userService) Signin(ctx context.Context, in *SigninReq, opts ...client.CallOption) (*AuthRsp, error) {
-	req := c.c.NewRequest(c.name, "UserService.Signin", in)
-	out := new(AuthRsp)
+func (c *userService) Signup(ctx context.Context, in *SignupReq, opts ...client.CallOption) (*UserInfo, error) {
+	req := c.c.NewRequest(c.name, "UserService.Signup", in)
+	out := new(UserInfo)
 	err := c.c.Call(ctx, req, out, opts...)
 	if err != nil {
 		return nil, err
@@ -64,9 +66,9 @@ func (c *userService) Signin(ctx context.Context, in *SigninReq, opts ...client.
 	return out, nil
 }
 
-func (c *userService) Signup(ctx context.Context, in *SignupReq, opts ...client.CallOption) (*UserInfo, error) {
-	req := c.c.NewRequest(c.name, "UserService.Signup", in)
-	out := new(UserInfo)
+func (c *userService) Signin(ctx context.Context, in *SigninReq, opts ...client.CallOption) (*AuthRsp, error) {
+	req := c.c.NewRequest(c.name, "UserService.Signin", in)
+	out := new(AuthRsp)
 	err := c.c.Call(ctx, req, out, opts...)
 	if err != nil {
 		return nil, err
@@ -84,22 +86,35 @@ func (c *userService) SigninByPhoneCaptcha(ctx context.Context, in *SigninByPhon
 	return out, nil
 }
 
+func (c *userService) Authorize(ctx context.Context, in *AuthorizeReq, opts ...client.CallOption) (*AuthRsp, error) {
+	req := c.c.NewRequest(c.name, "UserService.Authorize", in)
+	out := new(AuthRsp)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for UserService service
 
 type UserServiceHandler interface {
-	//用户登录(账号+密码)
-	Signin(context.Context, *SigninReq, *AuthRsp) error
 	//用户注册
 	Signup(context.Context, *SignupReq, *UserInfo) error
+	//用户登录(账号+密码)
+	Signin(context.Context, *SigninReq, *AuthRsp) error
 	//用户登录(手机号+验证码)
 	SigninByPhoneCaptcha(context.Context, *SigninByPhoneCaptchaReq, *AuthRsp) error
+	//第三方授权登陆 OAuth
+	Authorize(context.Context, *AuthorizeReq, *AuthRsp) error
 }
 
 func RegisterUserServiceHandler(s server.Server, hdlr UserServiceHandler, opts ...server.HandlerOption) error {
 	type userService interface {
-		Signin(ctx context.Context, in *SigninReq, out *AuthRsp) error
 		Signup(ctx context.Context, in *SignupReq, out *UserInfo) error
+		Signin(ctx context.Context, in *SigninReq, out *AuthRsp) error
 		SigninByPhoneCaptcha(ctx context.Context, in *SigninByPhoneCaptchaReq, out *AuthRsp) error
+		Authorize(ctx context.Context, in *AuthorizeReq, out *AuthRsp) error
 	}
 	type UserService struct {
 		userService
@@ -112,14 +127,18 @@ type userServiceHandler struct {
 	UserServiceHandler
 }
 
-func (h *userServiceHandler) Signin(ctx context.Context, in *SigninReq, out *AuthRsp) error {
-	return h.UserServiceHandler.Signin(ctx, in, out)
-}
-
 func (h *userServiceHandler) Signup(ctx context.Context, in *SignupReq, out *UserInfo) error {
 	return h.UserServiceHandler.Signup(ctx, in, out)
 }
 
+func (h *userServiceHandler) Signin(ctx context.Context, in *SigninReq, out *AuthRsp) error {
+	return h.UserServiceHandler.Signin(ctx, in, out)
+}
+
 func (h *userServiceHandler) SigninByPhoneCaptcha(ctx context.Context, in *SigninByPhoneCaptchaReq, out *AuthRsp) error {
 	return h.UserServiceHandler.SigninByPhoneCaptcha(ctx, in, out)
+}
+
+func (h *userServiceHandler) Authorize(ctx context.Context, in *AuthorizeReq, out *AuthRsp) error {
+	return h.UserServiceHandler.Authorize(ctx, in, out)
 }
