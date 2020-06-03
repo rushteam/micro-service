2a2b90e1ec70b151e471d5ac284561e9522132e9
mlboy <ml3@meitu.com>
2020-06-03 11:11:54 +0800

    del :diy jwt

--- service/user-srv/main.go
+++ service/user-srv/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"io/ioutil"
 	"time"
 
 	cli "github.com/micro/cli/v2"
@@ -9,6 +9,9 @@ import (
 	"github.com/micro/go-micro/v2/auth"
 	"github.com/micro/go-micro/v2/logger"
 	"github.com/rushteam/gosql"
+
+	// "github.com/rushteam/micro-service/common/micro/auth/jwt"
+	"github.com/micro/go-micro/v2/auth/jwt"
 	"github.com/rushteam/micro-service/common/micro/wrap"
 	"github.com/rushteam/micro-service/service/user-srv/handler"
 
@@ -26,21 +29,21 @@ var (
 )
 
 func main() {
-	//base64.StdEncoding.EncodeToString(
-	// privateKey, _ := ioutil.ReadFile("./key")
-	// publicKey, _ := ioutil.ReadFile("./key.pub")
-	// authd := jwt.NewAuth(
-	// 	auth.PrivateKey(string(privateKey)),
-	// 	auth.PublicKey(string(publicKey)),
-	// 	auth.LoginURL("UserService.Signup"),
-	// 	// auth.Exclude(excludeMethods...),
-	// )
+	// base64.StdEncoding.EncodeToString()
+	privateKey, _ := ioutil.ReadFile("./key")
+	publicKey, _ := ioutil.ReadFile("./key.pub")
+	authd := jwt.NewAuth(
+		auth.PrivateKey(string(privateKey)),
+		auth.PublicKey(string(publicKey)),
+		auth.LoginURL("UserService.Signup"),
+		// auth.Exclude(excludeMethods...),
+	)
 	srv := micro.NewService(
 		micro.RegisterTTL(time.Second*15),
 		micro.RegisterInterval(time.Second*5),
 		micro.Name(ServiceName),
 		micro.Version(ServiceVersion),
-		// micro.Auth(authd), //是否开启校验
+		micro.Auth(authd), //是否开启校验
 		micro.Flags(
 			&cli.StringFlag{
 				Name:    "config_path",
@@ -63,16 +66,16 @@ func main() {
 			return nil
 		}),
 	)
-	id := fmt.Sprintf("%v-%v", srv.Name(), srv.Options().Server.Options().Id)
-	acc, err := srv.Options().Auth.Generate(id, auth.WithScopes("user"))
-	if err != nil {
-		logger.Fatal(err)
-	}
-	tok, err := srv.Options().Auth.Token(auth.WithCredentials(acc.ID, acc.Secret))
-	if err != nil {
-		logger.Fatal(err)
-	}
-	srv.Options().Auth.Init(auth.ClientToken(tok))
+	// id := fmt.Sprintf("%v-%v", srv.Name(), srv.Options().Server.Options().Id)
+	// acc, err := srv.Options().Auth.Generate(id, auth.WithScopes("user"))
+	// if err != nil {
+	// 	logger.Fatal(err)
+	// }
+	// tok, err := srv.Options().Auth.Token(auth.WithCredentials(acc.ID, acc.Secret))
+	// if err != nil {
+	// 	logger.Fatal(err)
+	// }
+	// srv.Options().Auth.Init(auth.ClientToken(tok))
 	if err := srv.Run(); err != nil {
 		logger.Fatal(err)
 	}
